35010d9c7d78b47a7eeb0f76110729c1e31388e1
solkin <inbox@tomclaw.com>
2013-02-03 18:08:00 +0400

    Added PNG optimization script

---
+++ png_opt.sh
@@ -0,0 +1,5 @@
+pngcrush -rem allb -brute -reduce -ow -z 3 zmem 9 anim/res/smiles/*.png 
+optipng -o7 anim/res/smiles/*.png
+
+pngcrush -rem allb -brute -reduce -ow -z 3 zmem 9 stat/res/smiles/*.png 
+optipng -o7 stat/res/smiles/*.png
